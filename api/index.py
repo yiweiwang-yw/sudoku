3b97ce1fd3701251ff62a100afcce6b744779505
Yiweiwang <yiweiwangau@gmail.com>
2024-02-17 15:24:49 +1100

    Add myenv to .gitignore and update fastapi-dev script

--- api/index.py
+++ api/index.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# run myenv\Scripts\activate.bat for virtual environment on Windows
+
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
