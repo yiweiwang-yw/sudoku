014c7cf555a5fe5eefbfdab69985f6519922959d
Yiweiwang <yiweiwangau@gmail.com>
2024-05-13 21:57:47 +1000

    Added apiBaseURL

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ interface SudokuData {
 
 // const apiBaseUrl = "http://127.0.0.1:8000/api";
 
-const apiBaseUrl = "https://yw-sudoku.vercel.app/api";
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default function Home() {
     const [message, setMessage] = useState<string>("");
