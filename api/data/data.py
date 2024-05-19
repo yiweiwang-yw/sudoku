14e5c1faeff9988cb0095fe2f4dcab6b3807689a
Yiweiwang <yiweiwangau@gmail.com>
2024-05-19 16:19:16 +1000

    Test storing sudoku data into DynamoDB

--- api/data/data.py
+++ api/data/data.py
@@ -2,14 +2,13 @@ import enum
 import os
 import json
 import types
-
+import time
 
 class Difficulty(enum.Enum):
     low_difficulty = 1
     medium_difficulty = 5
     
 class title(enum.Enum):
-    first_column = 'id'
     second_column = 'puzzle'
     three_column = 'solution'
     four_column = 'clues'
@@ -18,7 +17,6 @@ class title(enum.Enum):
 
 def format_data(data: list) -> dict:
     formatted_data = {}
-    formatted_data[title.first_column.value] = int(data[0])
     puzzle_matrix = [list(data[1][i:i+9]) for i in range(0, 81, 9)]
     formatted_data[title.second_column.value] = puzzle_matrix
     solution_matrix = [list(data[2][i:i+9]) for i in range(0, 81, 9)]
@@ -37,13 +35,17 @@ def format_data(data: list) -> dict:
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 csv_file_path = os.path.join(script_dir, 'sudoku-3m.csv')
-json_file_path = os.path.join(script_dir, 'sudoku_test.json')
+json_file_path = os.path.join(script_dir, 'sudoku_full.json')
 
 low_difficulty_data = []
 medium_difficulty_data = []
 high_difficulty_data = []
 
 def process_data(format_data, csv_file_path):
+    start_time = time.time()
+    print('Processing data...')
+    total_number_of_lines = sum(1 for line in open(csv_file_path))
+    counter = 0
     with open(csv_file_path, 'r') as f:
         next(f)  # Skip the header line
         for line in f: 
@@ -52,25 +54,26 @@ def process_data(format_data, csv_file_path):
                 break
             data = line.split(',')
             formatted_data = format_data(data)
-    
-            if formatted_data['difficulty'] =='low' and len(low_difficulty_data) < 10:
-                low_difficulty_data.append(formatted_data)
-                print(formatted_data['difficulty'])
-            elif formatted_data['difficulty'] =='medium' and len(medium_difficulty_data) < 10:
-                medium_difficulty_data.append(formatted_data)
-                print(formatted_data['difficulty'])
-            elif formatted_data['difficulty'] == 'high' and len(high_difficulty_data) < 10:
-                high_difficulty_data.append(formatted_data)
-                print(formatted_data['difficulty'])
 
-# process_data(format_data, csv_file_path)
+            if formatted_data['difficulty'] =='low':
+                low_difficulty_data.append(formatted_data)
+            elif formatted_data['difficulty'] =='medium':
+                medium_difficulty_data.append(formatted_data)
+            elif formatted_data['difficulty'] == 'high':
+                high_difficulty_data.append(formatted_data)
+            counter += 1
+            print(f'Processed {counter} of {total_number_of_lines} lines')
+    end_time = time.time()
+    print(f'Processing data took {end_time - start_time} seconds')
+
+process_data(format_data, csv_file_path)
 
 def write_all_data_to_json(json_file_path):
     all_data = low_difficulty_data + medium_difficulty_data + high_difficulty_data
     with open(json_file_path, 'w') as write_file:
         json.dump(all_data, write_file)
 
-# write_all_data_to_json(json_file_path)
+write_all_data_to_json(json_file_path)
 
 
 
--- dynamo.py
+++ dynamo.py
@@ -1,7 +1,88 @@
 import boto3
+import json
+import os
+import uuid
 
-dynamodb = boto3.resource('dynamodb')
 
-table = dynamodb.Table('users')
+def create_sudoku_table():
+    # Initialize the DynamoDB client
+    dynamodb = boto3.resource('dynamodb')
 
-print(table.creation_date_time)
+    # Define the table schema
+    table = dynamodb.create_table(
+        TableName='SudokuPuzzles',
+        KeySchema=[
+            {
+                'AttributeName': 'puzzle_id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'puzzle_id',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'difficulty',
+                'AttributeType': 'S'
+            }
+        ],
+        GlobalSecondaryIndexes=[
+            {
+                'IndexName': 'difficulty-index',
+                'KeySchema': [
+                    {
+                        'AttributeName': 'difficulty',
+                        'KeyType': 'HASH'
+                    },
+                    {
+                        'AttributeName': 'puzzle_id',
+                        'KeyType': 'RANGE'
+                    }
+                ],
+                'Projection': {
+                    'ProjectionType': 'ALL'
+                },
+                'ProvisionedThroughput': {
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
+                }
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+
+    # Wait until the table exists
+    table.meta.client.get_waiter('table_exists').wait(
+        TableName='SudokuPuzzles')
+
+    print("Table created successfully.")
+
+
+
+
+def load_data_into_dynamodb(table):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    json_file_path = os.path.join(current_dir, 'api', 'data', 'sudoku_full.json')
+    with open(json_file_path, 'r') as f:
+        print('Loading data...')
+        sudoku_data = json.load(f)
+        # store first 10 sudoku puzzles in DynamoDB
+        print('Storing data...')
+        for i in range(10):
+            item = sudoku_data[i]
+            item['puzzle_id'] = str(uuid.uuid4())
+            table.put_item(Item=item)
+        print('Data loaded successfully!')
+
+
+
+if __name__ == '__main__':
+    # create_sudoku_table()
+    table = boto3.resource('dynamodb').Table('SudokuPuzzles')
+    load_data_into_dynamodb(table)
+    response = table.scan()
+    print(response['Items'])
