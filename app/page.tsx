162a020ff89c19c6c3bf6bee14debb4676341c52
Yiweiwang <yiweiwangau@gmail.com>
2024-05-22 16:28:34 +1000

    chore: Refactor Sudoku board rendering and user input handling

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import {
     DropdownItem,
     Button,
 } from "@nextui-org/react";
-const { DynamoDBClient, ListTablesCommand } = require("@aws-sdk/client-dynamodb");
 
 interface SudokuData {
     id: number;
@@ -18,14 +17,13 @@ interface SudokuData {
     difficulty: string;
 }
 
-// const apiBaseUrl = "http://127.0.0.1:8000/api";
-
 const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-
 
 export default function Home() {
     const [message, setMessage] = useState<string>("");
     const [puzzleData, setPuzzleData] = useState<SudokuData | null>(null);
+    const [userInputs, setUserInputs] = useState<string[][]>([]);
+    const [editedCells, setEditedCells] = useState<Set<string>>(new Set());
 
     async function fetchRandomSudoku() {
         try {
@@ -35,6 +33,7 @@ export default function Home() {
             const data: SudokuData = await response.json();
             setMessage("Puzzle loaded");
             setPuzzleData(data);
+            setUserInputs(data.puzzle.map((row) => row.slice()));
         } catch (error) {
             console.error("error", error);
             setMessage("Failed to load puzzle");
@@ -49,6 +48,7 @@ export default function Home() {
             const data: SudokuData = await response.json();
             setMessage(`Puzzle loaded with difficulty: ${difficulty}`);
             setPuzzleData(data);
+            setUserInputs(data.puzzle.map((row) => row.slice()));
         } catch (error) {
             console.error("error", error);
             setMessage("Failed to load puzzle");
@@ -61,6 +61,8 @@ export default function Home() {
 
     const initializeEmptyBoard = () => {
         const emptyBoard = Array.from({ length: 9 }, () => Array(9).fill("."));
+        setUserInputs(emptyBoard);
+        setEditedCells(new Set());
         setMessage("Board initialized");
         return {
             id: 0,
@@ -76,6 +78,29 @@ export default function Home() {
         setMessage("Board cleared");
     };
 
+    const handleInputChange = (
+        rowIndex: number,
+        colIndex: number,
+        value: string
+    ) => {
+        if (puzzleData) {
+            const updatedInputs = userInputs.map((row, rIndex) =>
+                row.map((cell, cIndex) => {
+                    if (rIndex === rowIndex && cIndex === colIndex) {
+                        return /^[1-9]$/.test(value) ? value : ".";
+                    }
+                    return cell;
+                })
+            );
+
+            const updatedEditedCells = new Set(editedCells);
+            updatedEditedCells.add(`${rowIndex}-${colIndex}`);
+
+            setUserInputs(updatedInputs);
+            setEditedCells(updatedEditedCells);
+        }
+    };
+
     const renderSudokuBoard = () => {
         if (!puzzleData) {
             return <div>Loading...</div>;
@@ -84,28 +109,48 @@ export default function Home() {
         return puzzleData.puzzle.map((row: string[], rowIndex: number) => (
             <div key={`row-${rowIndex}`} className="flex">
                 {row.map((value: string, colIndex: number) => (
-                    <div
+                    <input
                         key={`cell-${rowIndex}-${colIndex}`}
-                        className={`w-10 h-10 flex items-center justify-center border ${
-                            rowIndex % 3 === 2 && colIndex % 3 === 2
-                                ? "border-b-2 border-r-2"
+                        className={`w-10 h-10 flex items-center justify-center border text-black ${
+                            value !== "." &&
+                            !editedCells.has(`${rowIndex}-${colIndex}`)
+                                ? "text-blue-500"
+                                : userInputs[rowIndex][colIndex] !== "."
+                                ? "text-red-500"
                                 : ""
-                        } ${rowIndex === 8 ? "border-b-2" : ""} ${
-                            colIndex === 8 ? "border-r-2" : ""
                         }`}
                         style={{
-                            borderTop: rowIndex === 0 ? "2px solid" : "none",
-                            borderLeft: colIndex === 0 ? "2px solid" : "none",
+                            borderTop:
+                                rowIndex === 0 ? "2px solid black" : "none",
+                            borderLeft:
+                                colIndex === 0 ? "2px solid black" : "none",
                             borderBottom:
-                                rowIndex % 3 === 2 ? "2px solid" : "1px solid",
+                                rowIndex % 3 === 2
+                                    ? "2px solid black"
+                                    : "1px solid black",
                             borderRight:
-                                colIndex % 3 === 2 ? "2px solid" : "1px solid",
+                                colIndex % 3 === 2
+                                    ? "2px solid black"
+                                    : "1px solid black",
                             fontSize: "16px",
                             fontFamily: "monospace",
+                            textAlign: "center",
                         }}
-                    >
-                        {value !== "." ? value : ""}
-                    </div>
+                        type="text"
+                        value={
+                            userInputs[rowIndex][colIndex] !== "."
+                                ? userInputs[rowIndex][colIndex]
+                                : ""
+                        }
+                        onChange={(e) =>
+                            handleInputChange(
+                                rowIndex,
+                                colIndex,
+                                e.target.value
+                            )
+                        }
+                        maxLength={1}
+                    />
                 ))}
             </div>
         ));
@@ -128,10 +173,13 @@ export default function Home() {
                             Select Difficulty
                         </Button>
                     </DropdownTrigger>
-                    <DropdownMenu aria-label="Static Actions" onAction={(key) => fetchSudokuByDifficulty(String(key))}>
-                        <DropdownItem key="low">Simple</DropdownItem>
+                    <DropdownMenu
+                        aria-label="Static Actions"
+                        onAction={(key) => fetchSudokuByDifficulty(String(key))}
+                    >
+                        <DropdownItem key="low">Low</DropdownItem>
                         <DropdownItem key="medium">Medium</DropdownItem>
-                        <DropdownItem key="high">Hard</DropdownItem>
+                        <DropdownItem key="high">High</DropdownItem>
                     </DropdownMenu>
                 </Dropdown>
             </div>
